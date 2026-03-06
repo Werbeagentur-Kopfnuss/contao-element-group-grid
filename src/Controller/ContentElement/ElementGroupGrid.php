c1a1adfc19fe8928e02230b32c6efb42453341b0
Marco Schädlich <marco@werbeagentur-kopfnuss.de>
2026-03-06 13:55:02 +0100

    add new custom element

--- src/Controller/ContentElement/ElementGroupGrid.php
+++ src/Controller/ContentElement/ElementGroupGrid.php
@@ -11,7 +11,7 @@ use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsContentElement(category: 'miscellaneous', nestedFragments: ['allowedTypes' => ['image', 'text', 'player', 'table', 'headline', 'download', 'element_group', 'accordion', 'rsce_icon_text', 'rsce_page_teaser_standard', 'rsce_team', 'rsce_block_icon_text', 'rsce_link_list', 'rsce_link_list', 'rsce_button', 'rsce_block_image_slider_text', 'rsce_block_table_link', 'rsce_block_icon_text_link']], template: 'content_element/element_group_grid')]
+#[AsContentElement(category: 'miscellaneous', nestedFragments: ['allowedTypes' => ['image', 'text', 'player', 'table', 'headline', 'download', 'element_group', 'accordion', 'rsce_icon_text', 'rsce_page_teaser_standard', 'rsce_team', 'rsce_block_icon_text', 'rsce_link_list', 'rsce_link_list', 'rsce_button', 'rsce_block_image_slider_text', 'rsce_block_table_link', 'rsce_block_icon_text_link', 'rsce_block_image_animation_headline']], template: 'content_element/element_group_grid')]
 class ElementGroupGrid extends AbstractContentElementController
 {
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
